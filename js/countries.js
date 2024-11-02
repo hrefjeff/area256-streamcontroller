04142db445376810750b8b390fd80f402dd38639
buttersauce <ryankharvey@gmail.com>
2024-11-02 01:47:55 -0500

    Update countries.js
    Added way more countries

--- js/countries.js
+++ js/countries.js
@@ -25,7 +25,441 @@ var countries = {
 		"alpha3": "WOR",
 		"name": "World",
 		"flag": "WO"
-	}
+	},
+
+	"DE": {
+    	"alpha2": "DE",
+    	"alpha3": "DEU",
+    	"name": "Germany",
+    	"flag": "DE"
+	},
+
+	"AU": {
+    	"alpha2": "AU",
+    	"alpha3": "AUS",
+    	"name": "Australia",
+    	"flag": "AU"
+	},
+
+	"FR": {
+ 	   "alpha2": "FR",
+    	"alpha3": "FRA",
+    	"name": "France",
+    	"flag": "FR"
+    },
+
+    "JP": {
+ 	   "alpha2": "JP",
+    	"alpha3": "JPN",
+    	"name": "Japan",
+    	"flag": "JP"
+	},
+
+	"BR": {
+    	"alpha2": "BR",
+    	"alpha3": "BRA",
+    	"name": "Brazil",
+    	"flag": "BR"
+	},
+
+	"IN": {
+ 	   "alpha2": "IN",
+ 	   "alpha3": "IND",
+ 	   "name": "India",
+ 	   "flag": "IN"
+	},
+
+	"IT": {
+    	"alpha2": "IT",
+   		"alpha3": "ITA",
+   		"name": "Italy",
+    	"flag": "IT"
+	},
+
+	"MX": {
+    	"alpha2": "MX",
+    	"alpha3": "MEX",
+    	"name": "Mexico",
+    	"flag": "MX"
+	},
+
+	"ZA": {
+	    "alpha2": "ZA",
+    	"alpha3": "ZAF",
+    	"name": "South Africa",
+    	"flag": "ZA"
+	},
+
+	"RU": {
+ 	   "alpha2": "RU",
+ 	   "alpha3": "RUS",
+ 	   "name": "Russia",
+ 	   "flag": "RU"
+	},
+
+	"KR": {
+    	"alpha2": "KR",
+    	"alpha3": "KOR",
+    	"name": "South Korea",
+ 		"flag": "KR"
+	},
+
+	"ES": {
+    	"alpha2": "ES",
+    	"alpha3": "ESP",
+   		"name": "Spain",
+ 		"flag": "ES"
+	},
+
+	"NL": {
+    	"alpha2": "NL",
+    	"alpha3": "NLD",
+    	"name": "Netherlands",
+ 	   "flag": "NL"
+	},
+
+	"SE": {
+ 	   "alpha2": "SE",
+    	"alpha3": "SWE",
+    	"name": "Sweden",
+    	"flag": "SE"
+	},
+
+	"NO": {
+    	"alpha2": "NO",
+    	"alpha3": "NOR",
+    	"name": "Norway",
+ 	   "flag": "NO"
+	},
+
+	"AR": {
+    	"alpha2": "AR",
+    	"alpha3": "ARG",
+    	"name": "Argentina",
+    	"flag": "AR"
+	},
+
+	"TR": {
+    	"alpha2": "TR",
+    	"alpha3": "TUR",
+    	"name": "Turkey",
+   		"flag": "TR"
+	},
+
+	"ID": {
+    	"alpha2": "ID",
+    	"alpha3": "IDN",
+    	"name": "Indonesia",
+ 		"flag": "ID"
+	},
+
+	"TH": {
+    	"alpha2": "TH",
+    	"alpha3": "THA",
+    	"name": "Thailand",
+    	"flag": "TH"
+	},
+
+	"PH": {
+    	"alpha2": "PH",
+    	"alpha3": "PHL",
+    	"name": "Philippines",
+    	"flag": "PH"
+	},
+
+	"SA": {
+    	"alpha2": "SA",
+    	"alpha3": "SAU",
+    	"name": "Saudi Arabia",
+    	"flag": "SA"
+	},
+
+	"NG": {
+    	"alpha2": "NG",
+    	"alpha3": "NGA",
+    	"name": "Nigeria",
+    	"flag": "NG"
+	},
+
+	"NZ": {
+    	"alpha2": "NZ",
+    	"alpha3": "NZL",
+    	"name": "New Zealand",
+    	"flag": "NZ"
+	},
+	
+	"CH": {
+    	"alpha2": "CH",
+    	"alpha3": "CHE",
+    	"name": "Switzerland",
+    	"flag": "CH"
+	},
+
+	"AT": {
+    	"alpha2": "AT",
+    	"alpha3": "AUT",
+    	"name": "Austria",
+    	"flag": "AT"
+	},
+
+	"DK": {
+    	"alpha2": "DK",
+    	"alpha3": "DNK",
+    	"name": "Denmark",
+    	"flag": "DK"
+	},
+
+	"FI": {
+    	"alpha2": "FI",
+    	"alpha3": "FIN",
+    	"name": "Finland",
+    	"flag": "FI"
+	},
+
+	"SG": {
+    	"alpha2": "SG",
+    	"alpha3": "SGP",
+    	"name": "Singapore",
+    	"flag": "SG"
+	},
+
+	"EG": {
+    	"alpha2": "EG",
+    	"alpha3": "EGY",
+    	"name": "Egypt",
+    	"flag": "EG"
+	},
+
+	"MY": {
+    	"alpha2": "MY",
+    	"alpha3": "MYS",
+    	"name": "Malaysia",
+    	"flag": "MY"
+	},
+
+	"VN": {
+    	"alpha2": "VN",
+    	"alpha3": "VNM",
+    	"name": "Vietnam",
+    	"flag": "VN"
+	},
+
+	"CO": {
+    	"alpha2": "CO",
+    	"alpha3": "COL",
+    	"name": "Colombia",
+    	"flag": "CO"
+	},
+
+	"PT": {
+    	"alpha2": "PT",
+    	"alpha3": "PRT",
+    	"name": "Portugal",
+    	"flag": "PT"
+	},
+
+	"GR": {
+    	"alpha2": "GR",
+    	"alpha3": "GRC",
+    	"name": "Greece",
+    	"flag": "GR"
+	},
+
+	"HU": {
+    	"alpha2": "HU",
+    	"alpha3": "HUN",
+    	"name": "Hungary",
+    	"flag": "HU"
+	},
+
+	"CZ": {
+    	"alpha2": "CZ",
+    	"alpha3": "CZE",
+    	"name": "Czech Republic",
+    	"flag": "CZ"
+	},
+
+	"RO": {
+    	"alpha2": "RO",
+    	"alpha3": "ROU",
+    	"name": "Romania",
+    	"flag": "RO"
+	},
+
+	"IL": {
+    	"alpha2": "IL",
+    	"alpha3": "ISR",
+    	"name": "Israel",
+    	"flag": "IL"
+	},
+
+	"IE": {
+    	"alpha2": "IE",
+    	"alpha3": "IRL",
+    	"name": "Ireland",
+    	"flag": "IE"
+	},
+
+	"QA": {
+    	"alpha2": "QA",
+    	"alpha3": "QAT",
+    	"name": "Qatar",
+    	"flag": "QA"
+	},
+
+	"KZ": {
+    	"alpha2": "KZ",
+    	"alpha3": "KAZ",
+    	"name": "Kazakhstan",
+    	"flag": "KZ"
+	},
+
+	"BD": {
+    	"alpha2": "BD",
+    	"alpha3": "BGD",
+    	"name": "Bangladesh",
+    	"flag": "BD"
+	},
+
+	"CL": {
+    	"alpha2": "CL",
+    	"alpha3": "CHL",
+    	"name": "Chile",
+    	"flag": "CL"
+	},
+
+	"PE": {
+    	"alpha2": "PE",
+   		"alpha3": "PER",
+  	  	"name": "Peru",
+   	 	"flag": "PE"
+	},
+
+	"VE": {
+    	"alpha2": "VE",
+    	"alpha3": "VEN",
+   		"name": "Venezuela",
+    	"flag": "VE"
+	},
+
+	"SK": {
+    	"alpha2": "SK",
+    	"alpha3": "SVK",
+    	"name": "Slovakia",
+    	"flag": "SK"
+	},
+
+	"BG": {
+    	"alpha2": "BG",
+    	"alpha3": "BGR",
+    	"name": "Bulgaria",
+    	"flag": "BG"
+	},
+
+	"RS": {
+    	"alpha2": "RS",
+    	"alpha3": "SRB",
+    	"name": "Serbia",
+    	"flag": "RS"
+	},
+
+	"HR": {
+    	"alpha2": "HR",
+    	"alpha3": "HRV",
+    	"name": "Croatia",
+    	"flag": "HR"
+	},
+
+	"LU": {
+    	"alpha2": "LU",
+    	"alpha3": "LUX",
+    	"name": "Luxembourg",
+    	"flag": "LU"
+	},
+
+	"IS": {
+    	"alpha2": "IS",
+    	"alpha3": "ISL",
+    	"name": "Iceland",
+    	"flag": "IS"
+	},
+
+	"MT": {
+    	"alpha2": "MT",
+    	"alpha3": "MLT",
+    	"name": "Malta",
+    	"flag": "MT"
+	},
+
+	"EE": {
+    	"alpha2": "EE",
+    	"alpha3": "EST",
+    	"name": "Estonia",
+    	"flag": "EE"
+	},
+
+	"LV": {
+    	"alpha2": "LV",
+    	"alpha3": "LVA",
+    	"name": "Latvia",
+    	"flag": "LV"
+	},
+
+	"LT": {
+    	"alpha2": "LT",
+    	"alpha3": "LTU",
+    	"name": "Lithuania",
+    	"flag": "LT"
+	},
+
+	"CY": {
+    	"alpha2": "CY",
+    	"alpha3": "CYP",
+    	"name": "Cyprus",
+    	"flag": "CY"
+	},
+
+	"SI": {
+    	"alpha2": "SI",
+    	"alpha3": "SVN",
+    	"name": "Slovenia",
+    	"flag": "SI"
+	},
+
+	"MC": {
+    	"alpha2": "MC",
+    	"alpha3": "MCO",
+    	"name": "Monaco",
+    	"flag": "MC"
+	},
+
+	"SM": {
+    	"alpha2": "SM",
+    	"alpha3": "SMR",
+    	"name": "San Marino",
+    	"flag": "SM"
+	},
+
+	"AD": {
+    	"alpha2": "AD",
+    	"alpha3": "AND",
+    	"name": "Andorra",
+    	"flag": "AD"
+	},
+
+	"XK": {
+    	"alpha2": "XK",
+    	"alpha3": "XKX",
+    	"name": "Kosovo",
+    	"flag": "XK"
+	},
+
+	"BN": {
+    	"alpha2": "BN",
+    	"alpha3": "BRN",
+    	"name": "Brunei",
+    	"flag": "BN"
+	},
 };
 
 var countryFlag = function(country){
